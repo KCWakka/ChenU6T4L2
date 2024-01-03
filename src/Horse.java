d6b32472f31d7ba865c20a14c61a8d4052c0b198
BT_2N2_11 <sample@gmail.com>
2024-01-03 13:55:11 -0500

    done

--- src/Horse.java
+++ src/Horse.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Horse {
+public class Horse  {
+    private String name;
+    private int weight;
+
+    public Horse(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    /** @return the horse's name */
+    public String getName() {
+        return name;
+    }
+
+    /** @return the horse's weight */
+    public int getWeight() {
+        return weight;
+    }
+
+    /** @return a printable String of the Horse */
+    public String horseInfo() {
+        return "name: " + name + ", weight: " + weight;
+    }
 }
+
+
